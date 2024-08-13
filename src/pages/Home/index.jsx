942138160562097db4494b77bf48a9428fa18bc6
LukeSky25 <lucasskywalkerodrigues@gmail.com>
2024-08-12 21:18:42 -0300

    Estilização de slides e navbar

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -16,58 +16,59 @@ import 'swiper/css/scrollbar';
 export const Home = () => {
 
   const img = [
-    { id: 1, image: 'src/assets/construcao-civil-tendencias.webp' },
-    { id: 2, image: 'src/assets/silhuetas-do-local-de-construcao.avif' }
+    { id: 1, image: 'src/assets/banner.png' },
+    { id: 2, image: 'src/assets/construcao-civil-tendencias.webp' },
+    { id: 3, image: 'src/assets/silhuetas-do-local-de-construcao.avif' },
   ];
 
   return (
     <>
       <Header />
 
-          <Swiper
-            modules={[Navigation, Pagination, Autoplay]}
-            slidesPerView={1}
-            spaceBetween={30}
-            pagination={{ clickable: true }}
-            navigation
-            scrollbar={{ draggable: true }}
-            loop={true}
-            autoplay={{ delay: 6000 }}
-          >
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        slidesPerView={1}
+        spaceBetween={30}
+        pagination={{ clickable: true }}
+        navigation
+        scrollbar={{ draggable: true }}
+        loop={true}
+        autoplay={{ delay: 6000 }}
+      >
 
-            {img.map((item) => (
-              <SwiperSlide key={item.id}>
-                <img className='banner' src={item.image} alt={`Slide ${item.id}`} />
-              </SwiperSlide>
-            ))}
+        {img.map((item) => (
+          <SwiperSlide key={item.id}>
+            <img className='banner' src={item.image} alt={`Slide ${item.id}`} />
+          </SwiperSlide>
+        ))}
 
-            <SwiperSlide className='teste'>
-              <div>
-                <h1>Titulo</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error minima iure excepturi et distinctio voluptas, nisi, sequi, explicabo tempora perferendis magnam quidem in unde modi? Asperiores modi porro corporis rem.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nulla, non quo debitis libero explicabo excepturi cumque provident mollitia in distinctio tempore quam iure consequuntur quibusdam illum voluptate laborum maiores!</p>
-              </div>
-            </SwiperSlide>
+        <SwiperSlide className='banner-html banner-1'>
+          <div>
+            <h1>Titulo</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error minima iure excepturi et distinctio voluptas, nisi, sequi, explicabo tempora perferendis magnam quidem in unde modi? Asperiores modi porro corporis rem.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nulla, non quo debitis libero explicabo excepturi cumque provident mollitia in distinctio tempore quam iure consequuntur quibusdam illum voluptate laborum maiores!</p>
+          </div>
+        </SwiperSlide>
 
-            <SwiperSlide className='teste2'>
-              <div>
-                <h1>Titulo</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error minima iure excepturi et distinctio voluptas, nisi, sequi, explicabo tempora perferendis magnam quidem in unde modi? Asperiores modi porro corporis rem.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nulla, non quo debitis libero explicabo excepturi cumque provident mollitia in distinctio tempore quam iure consequuntur quibusdam illum voluptate laborum maiores!</p>
-              </div>
-            </SwiperSlide>
+        <SwiperSlide className='teste2'>
+          <div>
+            <h1>Titulo</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error minima iure excepturi et distinctio voluptas, nisi, sequi, explicabo tempora perferendis magnam quidem in unde modi? Asperiores modi porro corporis rem.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nulla, non quo debitis libero explicabo excepturi cumque provident mollitia in distinctio tempore quam iure consequuntur quibusdam illum voluptate laborum maiores!</p>
+          </div>
+        </SwiperSlide>
 
-          </Swiper>
+      </Swiper>
 
-        <section>
+      <section>
 
-          <main>
+        <main>
 
 
 
-          </main>
+        </main>
 
-        </section>
+      </section>
 
       <Footer />
     </>
