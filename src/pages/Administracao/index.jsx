70fc682ec9632d55d42b6e2826c843fa14d83854
LukeSky25 <lucasskywalkerodrigues@gmail.com>
2025-04-16 00:51:26 -0300

    Inicialização da Criação da Tabela de Administradores

--- src/pages/Administracao/index.jsx
+++ src/pages/Administracao/index.jsx
@@ -1,3 +1,5 @@
+import { FaEdit } from "react-icons/fa";
+
 import { Footer } from "../../components/Footer";
 import { Header2 } from "../../components/Header2";
 
@@ -10,7 +12,73 @@ export const Administracao = () => {
 
       <section>
         <main>
-          <h1 className="">Adm</h1>
+          <div className="container2">
+            <h1 className="adm_h1">Administradores</h1>
+
+            <table>
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Nome</th>
+                  <th>Usuário</th>
+                  <th>Ativo</th>
+                  <th>Ações</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="adms">
+                  <td>1</td>
+                  <td>Nome1</td>
+                  <td>Usuário1</td>
+                  <td>Sim</td>
+                  <td className="adm_acoes">
+                    <div className="adm_exclude_b">Excluir</div>
+                    <FaEdit size={30} className="adm_edit" />
+                  </td>
+                </tr>
+
+                <tr className="adms">
+                  <td>1</td>
+                  <td>Nome1</td>
+                  <td>Usuário1</td>
+                  <td>Sim</td>
+                  <td>
+                    <p></p>
+                  </td>
+                </tr>
+
+                <tr className="adms">
+                  <td>1</td>
+                  <td>Nome1</td>
+                  <td>Usuário1</td>
+                  <td>Sim</td>
+                  <td>
+                    <p></p>
+                  </td>
+                </tr>
+
+                <tr className="adms">
+                  <td>1</td>
+                  <td>Nome1</td>
+                  <td>Usuário1</td>
+                  <td>Sim</td>
+                  <td>
+                    <p></p>
+                  </td>
+                </tr>
+
+                <tr className="adms">
+                  <td>1</td>
+                  <td>Nome1</td>
+                  <td>Usuário1</td>
+                  <td>Sim</td>
+                  <td>
+                    <p></p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </main>
       </section>
 
