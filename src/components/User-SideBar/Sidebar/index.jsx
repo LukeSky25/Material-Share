5edeb24e5b83dd21b580c78f201f2522442398ab
Lucas Lucena Rodrigues dos Santos <rm89997@estudante.fieb.edu.br>
2025-09-11 09:07:56 -0300

    Correção de sidebar e criação de conexeão de Pessoa

--- src/components/User-SideBar/Sidebar/index.jsx
+++ src/components/User-SideBar/Sidebar/index.jsx
@@ -1,4 +1,9 @@
+import React, { useState, useEffect } from "react";
 import { Container, Content } from "./styles";
+import { Link } from "react-router-dom";
+import SidebarItem from "../SidebarItem";
+import { isUserLoggedIn } from "../../../auth/authService";
+
 import {
   FaTimes,
   FaRegHeart,
@@ -7,33 +12,45 @@ import {
   FaHeadphonesAlt,
   FaRegBell,
 } from "react-icons/fa";
-import SidebarItem from "../SidebarItem";
-import { Link } from "react-router-dom";
 
 const Sidebar = ({ isOpen, active }) => {
+  const [user, setUser] = useState({ loggedIn: false, data: null });
+
   const closeSidebar = () => {
     active(false);
   };
+
+  useEffect(() => {
+    const usuario = isUserLoggedIn();
+    setUser(usuario);
+  }, []);
 
   return (
     <Container $isOpen={isOpen}>
       <FaTimes onClick={closeSidebar} />
       <Content>
-        <Link to="/user">
-          <SidebarItem Icon={FaUserAlt} Text="Usuário" />
-        </Link>
-        <Link to="/user/notificacao">
-          <SidebarItem Icon={FaRegBell} Text="Notificações" />
-        </Link>
-        <Link to="/user/suporte">
-          <SidebarItem Icon={FaHeadphonesAlt} Text="Suporte" />
-        </Link>
-        <Link to="/user/avaliacao">
-          <SidebarItem Icon={FaRegHeart} Text="Avaliação" />
-        </Link>
-        <Link to="/user/logout">
-          <SidebarItem Icon={FaSignInAlt} Text="Logout" />
-        </Link>
+       
+        {user.loggedIn && user.data ? (
+          <>
+            <Link to={`/user/${user.data.id}`}>
+              <SidebarItem Icon={FaUserAlt} Text="Usuário" />
+            </Link>
+            <Link to="/user/notificacao">
+              <SidebarItem Icon={FaRegBell} Text="Notificações" />
+            </Link>
+            <Link to="/user/suporte">
+              <SidebarItem Icon={FaHeadphonesAlt} Text="Suporte" />
+            </Link>
+            <Link to="/user/avaliacao">
+              <SidebarItem Icon={FaRegHeart} Text="Avaliação" />
+            </Link>
+            <Link to="/user/logout">
+              <SidebarItem Icon={FaSignInAlt} Text="Logout" />
+            </Link>
+          </>
+        ) : (
+          <p>Carregando menu...</p>
+        )}
       </Content>
     </Container>
   );
---
+++ src/services/PessoaService.js
@@ -0,0 +1,53 @@
+import http from "../common/http-common";
+
+const API_URL = "pessoa/";
+
+const findAll = () => {
+  return http.mainInstace.get(`${API_URL}findAll`);
+};
+
+const findById = (id) => {
+  return http.mainInstace.get(`${API_URL}findById/${id}`);
+};
+
+const findByTipo = (tipo) => {
+    return http.mainInstace.get(`${API_URL}findByTipo/${tipo}`)
+}
+
+const save = data => {
+    const formData = new FormData();
+    formData.append('nome', data.nome)
+    formData.append('dataNascimento', data.dataNascimento)
+    formData.append('cpf_cnpj', data.cpf_cnpj)
+    formData.append('numeroResidencia', data.numeroResidencia)
+    formData.append('tipo', data.tipo)
+    formData.append('usuario_id', data.usuario_id)
+    formData.append('statusDoador', "ATIVO")
+
+    return http.mainInstace.post(API_URL + "save", formData)
+}
+
+const editar = (id, data) => {
+    const formData = new FormData()
+
+    formData.append('nome', data.nome)
+    formData.append('dataNascimento', data.dataNascimento)
+    formData.append('cpf_cnpj', data.cpf_cnpj)
+    formData.append('numeroResidencia', data.numeroResidencia)
+    formData.append('tipo', data.tipo)
+    formData.append('usuario_id', data.usuario_id)
+    formData.append('statusDoador', "ATIVO")
+
+    return http.mainInstace.put(API_URL + `editar/${id}`, formData)
+}
+
+
+const usuarioService = {
+  findAll,
+  findById,
+  findByTipo,
+  save,
+  editar
+};
+
+export default usuarioService;
